108cf0e00fc8c5c80dd0946285a323235ba0bbaa
muhammed <mohammedzuhair4112003@gmail.com>
2024-12-07 17:31:18 +0300

    first-commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello, $name</h1>
-</body>
-</html>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Hello, {{ $name }}</h1>
+            <form action="about" method="post">
+    @csrf 
+    <input type="text" name="name" id="name"><br><br>
+    <select name="department" id="department">
+        @foreach ($departments as $id => $department)
+            <option value="{{ $id }}">{{ $department }}</option>
+        @endforeach
+    </select>
+    <br><br>
+    <input type="submit" value="Send">
+</form>
+        </body>
+        </html>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('git branch
-/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about',Function(){
-    $name='mohammed';
-return view ('about')->with('name',$name);
+
+// GET request for 'about' page
+Route::get('/about', function () {
+    $name = 'Mohammed';
+    $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
+
+// POST request for form submission
+Route::post('/about', function () {
+    $name = request('name'); // Use Laravel's `request()` helper
+    $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
+Route::get('tasks',Function(){
+    return view('tasks');
+});
+Route::post('create',function(){
+    $task_name=$_POST['name'];
+DB::table('tasks')-> insert(['name' => $task_name]);
+    return view('tasks');
 });
